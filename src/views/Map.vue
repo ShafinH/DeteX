547725c34a2e973192936b24d1cdc4074de708eb
ShafinH <65793752+ShafinH@users.noreply.github.com>
2020-12-01 22:08:38 -0800

    Update Map.vue

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -8,31 +8,23 @@ export default {
     map: null,
     request: {},
   }),
-/*
+
   mounted() {
-    var map = this.map;
-    var center = new window.google.maps.LatLng(37.268108, -122.019447);
-    map = new window.google.maps.Map(document.getElementById("map"), {
+  this.initMap();
+
+  },
+
+  methods: {
+    initMap() { 
+        var center = new window.google.maps.LatLng(37.268108, -122.019447);
+    this.map = new window.google.maps.Map(document.getElementById("map"), {
       center: center,
       zoom: 15,
     });
-
-    function callBack(results, status) {
-      if (status == window.google.maps.places.PlacesService.OK) {
-        for (var i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-        }
-      }
     }
 
-    function createMarker(place) {
-      var placeLoc = place.geometry.location;
-      var marker = new window.google.maps.marker({
-        map: window.map,
-        position: place.geometry.location,
-      });
-    }
-  }, */
+  }
+  
 }; 
 </script>
 
